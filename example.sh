e40016ff2fe9aad5e9d02a1dc6121db7ab3d2d50
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2021-02-04 19:14:07 +0300

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-mkdir -p openport/DEBIAN
-mkdir -p openport/usr/bin
-touch openport/usr/bin/openport
-chmod 777 openport/usr/bin/openport
-cd openport
-nano DEBIAN/control
-nano openport/usr/bin/openport
-fakeroot dpkg-deb --build openport
-mv openport.deb openport_1.0-1_all.deb
+package="debmaker"
+version="1.0-3"
+chmod 777 $package/bin/$package
+fakeroot dpkg-deb --build $package
+mv $package".deb" $package"_"$version"_all.deb"
